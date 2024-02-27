5e37a5d34567df8202e3aa786568019d5e8dff62
Svetlana Khlobustova <37548939+SvetlanaKhlobustova50809@users.noreply.github.com>
2024-02-27 17:39:37 +0300

    Update lab02.cpp

--- prj.lab/lab02/lab02.cpp
+++ prj.lab/lab02/lab02.cpp
@@ -32,10 +32,10 @@ Mat drawHistogram(const Mat& image) {
 
 Mat addNoise(const Mat& image, double stddev) {
     Mat noisyImage = image.clone();
-    Mat noise(image.size(), CV_8U);
-    randn(noise, Scalar::all(0), Scalar::all(stddev));
-    noisyImage += noise;
-    return noisyImage;
+    Mat im = image.clone();
+    RNG rng;
+    rng.fill(noisyImage, RNG::NORMAL, stddev, stddev);
+    return im + noisyImage;
 }
 
 int main() {
@@ -54,51 +54,67 @@ int main() {
     Mat testImage3 = generateTestImage(side, innerSquareSide, circleRadius, brightnessLevels3);
     Mat testImage4 = generateTestImage(side, innerSquareSide, circleRadius, brightnessLevels4);
 
+    // Создание окна для вывода результатов
+    Mat combinedImages;
+
+    // Склейка исходных изображений
+    Mat combinedTop;
+    hconcat(testImage1, testImage2, combinedTop);
+    hconcat(combinedTop, testImage3, combinedTop);
+    hconcat(combinedTop, testImage4, combinedTop);
+
     // Значения среднеквадратичного отклонения для шума
     vector<double> stddevValues = { 3.0, 7.0, 15.0 };
 
     // Генерация зашумленных изображений и их гистограмм
-    vector<Mat> noisyTestImages;
-    vector<Mat> histImages;
+    vector<Mat> noisyTestImages(4);
+    vector<Mat> histImages(4);
+    Mat combinedBottom;
+    Mat combinedHistograms;
     for (double stddev : stddevValues) {
         Mat noisyTestImage1 = addNoise(testImage1, stddev);
         Mat noisyTestImage2 = addNoise(testImage2, stddev);
         Mat noisyTestImage3 = addNoise(testImage3, stddev);
         Mat noisyTestImage4 = addNoise(testImage4, stddev);
 
-        noisyTestImages.push_back(noisyTestImage1);
-        noisyTestImages.push_back(noisyTestImage2);
-        noisyTestImages.push_back(noisyTestImage3);
-        noisyTestImages.push_back(noisyTestImage4);
+        noisyTestImages[0] = (noisyTestImage1);
+        noisyTestImages[1] = (noisyTestImage2);
+        noisyTestImages[2] = (noisyTestImage3);
+        noisyTestImages[3] = (noisyTestImage4);
 
         Mat histImage1 = drawHistogram(noisyTestImage1);
         Mat histImage2 = drawHistogram(noisyTestImage2);
         Mat histImage3 = drawHistogram(noisyTestImage3);
         Mat histImage4 = drawHistogram(noisyTestImage4);
 
-        histImages.push_back(histImage1);
-        histImages.push_back(histImage2);
-        histImages.push_back(histImage3);
-        histImages.push_back(histImage4);
+        histImages[0] = (histImage1);
+        histImages[1] = (histImage2);
+        histImages[2] = (histImage3);
+        histImages[3] = (histImage4);
+
+        // Склейка зашумленных изображений и их гистограмм
+        
+        hconcat(noisyTestImages[0], noisyTestImages[1], combinedBottom);
+        hconcat(combinedBottom, noisyTestImages[2], combinedBottom);
+        hconcat(combinedBottom, noisyTestImages[3], combinedBottom);
+        //imshow("", combinedBottom);
+        //waitKey(10000);
+
+       
+        hconcat(histImages[0], histImages[1], combinedHistograms);
+        hconcat(combinedHistograms, histImages[2], combinedHistograms);
+        hconcat(combinedHistograms, histImages[3], combinedHistograms);
+
+        // Склейка исходных изображений и зашумленных с гистограммами
+        vconcat(combinedTop, combinedBottom, combinedImages);
+        vconcat(combinedImages, combinedHistograms, combinedImages);
+
+        combinedTop = combinedImages;
     }
 
-    // Соединение изображений в одно окно
-    Mat combinedTop;
-    hconcat(noisyTestImages[0], noisyTestImages[1], combinedTop);
-    hconcat(combinedTop, noisyTestImages[2], combinedTop);
-    hconcat(combinedTop, noisyTestImages[3], combinedTop);
-
-    Mat combinedBottom;
-    hconcat(histImages[0], histImages[1], combinedBottom);
-    hconcat(combinedBottom, histImages[2], combinedBottom);
-    hconcat(combinedBottom, histImages[3], combinedBottom);
-
-    Mat combined;
-    vconcat(combinedTop, combinedBottom, combined);
-
-    // Отображение сгенерированных изображений
-    imshow("Combined Images", combined);
-    waitKey(10000);
+    string outputFileName = "C:/Users/Svt/Desktop/khlobustova/prj.lab/lab02/combined_image.png";
+    imwrite(outputFileName, combinedImages);
+    cout << "Combined image saved as " << outputFileName << endl;
 
     return 0;
 }
