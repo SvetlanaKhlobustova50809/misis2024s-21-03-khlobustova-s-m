b783bde1491ff7955d6921ef3b3b072d24c3beb4
Svetlana Khlobustova <37548939+SvetlanaKhlobustova50809@users.noreply.github.com>
2024-06-24 11:04:34 +0300

    Update lab03.cpp

--- prj.lab/lab03/lab03.cpp
+++ prj.lab/lab03/lab03.cpp
@@ -3,6 +3,7 @@
 
 using namespace cv;
 using namespace std;
+bool f = true;
 
 Mat autoContrastSingleChannel(Mat img) {
     if (img.depth() != CV_8U) {
@@ -10,24 +11,17 @@ Mat autoContrastSingleChannel(Mat img) {
         img.convertTo(img, CV_8U);
     }
 
-    // Создание объекта CLAHE
-    Ptr<CLAHE> clahe = createCLAHE();
-    clahe->setClipLimit(4.0); // Установка ограничения контраста
-
-    Mat claheResult;
-    clahe->apply(img, claheResult);
-
     Mat hist;
     int histSize = 256;
     float range[] = { 0, 256 };
     const float* histRange = { range };
     bool uniform = true, accumulate = false;
 
-    calcHist(&claheResult, 1, 0, Mat(), hist, 1, &histSize, &histRange, uniform, accumulate);
+    calcHist(&img, 1, 0, Mat(), hist, 1, &histSize, &histRange, uniform, accumulate);
 
     float alpha = 0.15, beta = 0.0001;
     float sum = 0;
-    int totalPixels = claheResult.rows * claheResult.cols;
+    int totalPixels = img.rows * img.cols;
     int blackThreshold = 0, whiteThreshold = 255;
 
     for (int i = 0; i < histSize; i++) {
@@ -51,18 +45,156 @@ Mat autoContrastSingleChannel(Mat img) {
     img.convertTo(result, CV_8U, 255.0 / (whiteThreshold - blackThreshold), -255.0 * blackThreshold / (whiteThreshold - blackThreshold));
     return result;
 }
+//
+//Mat autoContrastJoint(Mat img) {
+//    if (img.depth() != CV_8U) {
+//        cout << "Image depth is not 8-bit. Converting to 8-bit depth." << endl;
+//        img.convertTo(img, CV_8U);
+//    }
+//
+//    // Создаем объединенную гистограмму
+//    vector<Mat> channels;
+//    split(img, channels);
+//
+//    Mat combinedHist = Mat::zeros(256, 1, CV_32F);
+//    for (int i = 0; i < channels.size(); i++) {
+//        Mat hist;
+//        int histSize = 256;
+//        float range[] = { 0, 256 };
+//        const float* histRange = { range };
+//        bool uniform = true, accumulate = false;
+//
+//        calcHist(&channels[i], 1, 0, Mat(), hist, 1, &histSize, &histRange, uniform, accumulate);
+//        combinedHist += hist;
+//    }
+//
+//    // Определяем старые пороги (clow и chigh)
+//    float alpha = 0.15, beta = 0.0001;
+//    float sum = 0;
+//    int totalPixels = img.rows * img.cols * img.channels();
+//    int clow = 0, chigh = 255;
+//
+//    for (int i = 0; i < 256; i++) {
+//        sum += combinedHist.at<float>(i);
+//        if (sum >= alpha * totalPixels) {
+//            clow = i;
+//            break;
+//        }
+//    }
+//
+//    sum = 0;
+//    for (int i = 255; i >= 0; i--) {
+//        sum += combinedHist.at<float>(i);
+//        if (sum >= beta * totalPixels) {
+//            chigh = i;
+//            break;
+//        }
+//    }
+//
+//    // Устанавливаем новые пороги (cmin и cmax)
+//    int cmin = 0, cmax = 255;
+//
+//    // Применяем автоконтрастирование
+//    img.convertTo(img, CV_32F);
+//    Mat result = img.clone();
+//    result.forEach<Vec3f>([&](Vec3f& pixel, const int* position) -> void {
+//        for (int i = 0; i < 3; i++) {
+//            pixel[i] = cmin + (pixel[i] - clow) * (cmax - cmin) / (chigh - clow);
+//            pixel[i] = std::min(std::max(pixel[i], static_cast<float>(cmin)), static_cast<float>(cmax)); // Ограничение значений диапазоном [cmin, cmax]
+//        }
+//        });
+//    result.convertTo(result, CV_8U);
+//
+//    return result;
+//}
 
-Mat autoContrastColor(Mat img) {
-    vector<Mat> channels;
-    split(img, channels);
+//Mat autoContrastColor(Mat img) {
+//    vector<Mat> channels;
+//    split(img, channels);
+//
+//    for (int i = 0; i < channels.size(); i++) {
+//        channels[i] = autoContrastSingleChannel(channels[i]);
+//    }
+//
+//    Mat result;
+//    merge(channels, result);
+//    return result;
+//}
 
-    for (int i = 0; i < channels.size(); i++) {
-        channels[i] = autoContrastSingleChannel(channels[i]);
+Mat autoContrastColor(Mat img, bool useJointContrast = false) {
+    if (useJointContrast) {
+        if (img.depth() != CV_8U) {
+            cout << "Image depth is not 8-bit. Converting to 8-bit depth." << endl;
+            img.convertTo(img, CV_8U);
+        }
+
+        // Создаем объединенную гистограмму
+        vector<Mat> channels;
+        split(img, channels);
+
+        Mat combinedHist = Mat::zeros(256, 1, CV_32F);
+        for (int i = 0; i < channels.size(); i++) {
+            Mat hist;
+            int histSize = 256;
+            float range[] = { 0, 256 };
+            const float* histRange = { range };
+            bool uniform = true, accumulate = false;
+
+            calcHist(&channels[i], 1, 0, Mat(), hist, 1, &histSize, &histRange, uniform, accumulate);
+            combinedHist += hist;
+        }
+
+        // Определяем старые пороги (clow и chigh)
+        float alpha = 0.15, beta = 0.0001;
+        float sum = 0;
+        int totalPixels = img.rows * img.cols * img.channels();
+        int clow = 0, chigh = 255;
+
+        for (int i = 0; i < 256; i++) {
+            sum += combinedHist.at<float>(i);
+            if (sum >= alpha * totalPixels) {
+                clow = i;
+                break;
+            }
+        }
+
+        sum = 0;
+        for (int i = 255; i >= 0; i--) {
+            sum += combinedHist.at<float>(i);
+            if (sum >= beta * totalPixels) {
+                chigh = i;
+                break;
+            }
+        }
+
+        // Устанавливаем новые пороги (cmin и cmax)
+        int cmin = 0, cmax = 255;
+
+        // Применяем автоконтрастирование
+        img.convertTo(img, CV_32F);
+        Mat result = img.clone();
+        result.forEach<Vec3f>([&](Vec3f& pixel, const int* position) -> void {
+            for (int i = 0; i < 3; i++) {
+                pixel[i] = cmin + (pixel[i] - clow) * (cmax - cmin) / (chigh - clow);
+                pixel[i] = std::min(std::max(pixel[i], static_cast<float>(cmin)), static_cast<float>(cmax)); // Ограничение значений диапазоном [cmin, cmax]
+            }
+            });
+        result.convertTo(result, CV_8U);
+
+        return result;
     }
+    else {
+        vector<Mat> channels;
+        split(img, channels);
 
-    Mat result;
-    merge(channels, result);
-    return result;
+        for (int i = 0; i < channels.size(); i++) {
+            channels[i] = autoContrastSingleChannel(channels[i]);
+        }
+
+        Mat result;
+        merge(channels, result);
+        return result;
+    }
 }
 
 void showHistogram(Mat& img) {
@@ -100,11 +232,15 @@ void showHistogram(Mat& img) {
     }
 
     imshow("Histogram", histImage);
+
+    if (f) {
+        string outputFileName = "C:/Users/Svt/Desktop/ProcImage/pictures/lab03_gist2.png";
+        imwrite(outputFileName, histImage);
+    }
 }
 
 int main(int argc, char* argv[]) {
-    Mat img = imread("C:/Users/Svt/Desktop/khlobustova/prj.lab/pictures/test_image.jpg");
-
+    Mat img = imread("C:/Users/Svt/Desktop/ProcImage/pictures/lab03_test2.jpg");
 
     if (img.empty()) {
         cout << "Could not open or find the image" << endl;
@@ -116,13 +252,16 @@ int main(int argc, char* argv[]) {
         result = autoContrastSingleChannel(img);
     }
     else {
-        result = autoContrastColor(img);
+        bool useJointContrast = true; // Или false, если хотите использовать поканальное контрастирование
+        result = autoContrastColor(img, useJointContrast);
     }
     showHistogram(img);
     showHistogram(result);
 
     imshow("Original Image", img);
     imshow("Auto-Contrast Image", result);
-    waitKey(10000);
+    string outputFileName = "C:/Users/Svt/Desktop/ProcImage/pictures/lab03_res2.png";
+    imwrite(outputFileName, result);
+    waitKey(100000);
 
 }
